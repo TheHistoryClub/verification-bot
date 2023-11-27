c81d42edc507e90e677a6eaa901f25290d3fe297
Eason Lu <ghcodesign@outlook.com>
2023-11-27 12:26:38 -0800

    Update deploy.sh
    Signed-off-by: Eason Lu <ghcodesign@outlook.com>

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Starting the container"
-docker run myapp:latest
+docker run -d verfication-bot
 sleep 60
 echo "Sending SIGKILL to container"
-docker stop --signal SIGKILL myapp:latest
+docker stop --signal SIGKILL verfication-bot
